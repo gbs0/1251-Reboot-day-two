a709185ed2c71f67bf25ed835ff5c8fe114a4c65
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2023-04-25 14:45:12 -0300

    automatted commit

--- app.rb
+++ app.rb
@@ -20,5 +20,8 @@ end
 # 8.1 Receber a lista de presentes p/ mostrar na tela  
 # 8.2 Mostrar os itens da lista (nome - preço) de forma ordenada e númerada
 def list(gift_list)
-
+  # 1 - Playstation 5 | $ 5000
+  gift_list.each_with_index do |gift, index|
+    puts "#{index + 1} - #{gift[:name]} | $ #{gift[:price]}"
+  end
 end
